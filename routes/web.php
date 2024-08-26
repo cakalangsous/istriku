512b5e583f0d179b40d819e8009f80c987ce3143
benny-unicomi <benny.kindangen@unicomi.com>
2024-08-26 19:40:55 +0800

    ready to use

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::middleware('guest')->group(function () {
+    Route::get('admin/login', [AuthenticatedSessionController::class, 'create'])
+        ->name('admin.login');
+
+    Route::post('admin/login', [AuthenticatedSessionController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('admin/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('admin.logout');
+});
