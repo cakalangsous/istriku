1cedb9cb1ccf7f8b78eba0fbabb6b23a3034b0a4
Benkin <bennykindangen@gmail.com>
2025-01-28 18:24:01 +0800

    updated design, updated filesystems

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Mazer Admin Dashboard</title>
+    <title>Login - {{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="{{ asset("assets/css/main/app.css") }}">
     <link rel="stylesheet" href="{{ asset("assets/css/pages/auth.css") }}">
     <link rel="shortcut icon" href="{{ asset("assets/images/logo/favicon.svg") }}" type="image/x-icon">
@@ -15,13 +15,18 @@
     <div id="auth">
         
         <div class="row h-100">
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
                         <a href=""><img src="{{ asset("assets/images/logo/logo.svg") }}" alt="Logo"></a>
                     </div>
                     <h1 class="auth-title">Log in</h1>
-                    <p class="auth-subtitle mb-4">Log in with your data that you entered during registration.</p>
+                    <p class="auth-subtitle mb-4">Please login to access the admin portal.</p>
 
                     @error('login')
                         <div class="alert alert-light-danger color-danger">
@@ -59,11 +64,6 @@
                         </div>
                         <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
                     </form>
-                </div>
-            </div>
-            <div class="col-lg-7 d-none d-lg-block">
-                <div id="auth-right">
-
                 </div>
             </div>
         </div>
